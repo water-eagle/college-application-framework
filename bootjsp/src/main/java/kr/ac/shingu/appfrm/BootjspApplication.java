076eafa5b59e6edbfd2e10ce165e9cb2b636a4db
water-eagle <130212192+water-eagle@users.noreply.github.com>
2026-09-09 14:03:27 +0900

    add: bootjsp 프로젝트 생성

---
+++ bootjsp/src/main/java/kr/ac/shingu/appfrm/BootjspApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.shingu.appfrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootjspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootjspApplication.class, args);
+	}
+
+}
